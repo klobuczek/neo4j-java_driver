5b9e8af70c540e833680cd07d8b03f2f9d3e37c6
Heinrich Klobuczek <heinrich@mail.com>
2018-08-18 14:39:35 +0200

    updated java driver jar

--- lib/neo4j-java_driver_jars.rb
+++ lib/neo4j-java_driver_jars.rb
@@ -2,7 +2,7 @@
 begin
   require 'jar_dependencies'
 rescue LoadError
-  require 'org/neo4j/driver/neo4j-java-driver/1.6.1/neo4j-java-driver-1.6.1.jar'
+  require 'org/neo4j/driver/neo4j-java-driver/1.6.2/neo4j-java-driver-1.6.2.jar'
 end
 
-require_jar 'org.neo4j.driver', 'neo4j-java-driver', '1.6.1' if defined? Jars
+require_jar 'org.neo4j.driver', 'neo4j-java-driver', '1.6.2' if defined? Jars
